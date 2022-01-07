db691ed55d9e87281cb08d163a3976b94e60b63a
il8677 <jiajiaisaac@gmail.com>
2022-01-08 09:08:54 +1100

    Updated tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,12 +3,54 @@
 
 #include "catch.hpp" 
 #include "../cppmidi.h"
+#include <cstddef>
 
 
-TEST_CASE("Header read correctly", "[header]"){
-	midi::MIDI rondo("rondo.mid");
+TEST_CASE("Header struct makes sense", "[header]"){
+	CHECK(offsetof(midi::Header, type)==0);
+	CHECK(offsetof(midi::Header, numTracks)==2);
+	CHECK(offsetof(midi::Header, ticksPerQuater)==4);
+}
 
-	REQUIRE(rondo.getHeader().type == midi::MULTI);
-	REQUIRE(rondo.getHeader().numTracks == 2);
-		
+TEST_CASE("Bad magic fails to load", "[header][loading]"){
+	midi::MIDI m;
+	REQUIRE_FALSE(m.loadFile("f.0.1.1284"));
+}
+
+TEST_CASE("Non existant file fails to load", "[loading]"){
+	midi::MIDI m;
+	REQUIRE_FALSE(m.loadFile(""));
+	REQUIRE_FALSE(m.loadFile("TTTTTTTTTTTTTT"));
+}
+
+TEST_CASE("Correct header loads", "[header][loading]"){
+	midi::MIDI m;
+	REQUIRE(m.loadFile("c.1.1.1284"));
+	REQUIRE(m.loadFile("c.0.1.9"));
+	REQUIRE(m.loadFile("c.1.4.1284"));
+}
+
+TEST_CASE("Type loads", "[header][loading]"){
+	midi::MIDI m;
+	m.loadFile("c.1.1.1284");
+	REQUIRE(m.getHeader().type == 1);
+	m.loadFile("c.0.1.9");
+	REQUIRE(m.getHeader().type == 0);
+}
+
+TEST_CASE("Num Tracks Load", "[header][loading]"){
+	midi::MIDI m;
+	m.loadFile("c.1.1.1284");
+	REQUIRE(m.getHeader().numTracks == 1);
+
+	m.loadFile("c.1.4.1284");
+	REQUIRE(m.getHeader().numTracks == 4);
+}
+
+TEST_CASE("Num Ticks Load", "[header][loading]"){
+	midi::MIDI m;
+	m.loadFile("c.1.1.1284");
+	REQUIRE(m.getHeader().ticksPerQuater == 1284);
+	m.loadFile("c.0.1.9");
+	REQUIRE(m.getHeader().ticksPerQuater == 9);
 }
